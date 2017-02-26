a461a690054cdc69c1bb16d24c00642490324a88
Aleksei <Dark_ALEX@sibmail.com>
2017-02-26 08:07:55 +0700

    add credit card

--- script.lua
+++ script.lua
@@ -11748,7 +11748,7 @@ function saveserver(thePlayer, x,y,z,rx,ry,rz)
 		datess=datess..'['..i..'] = {'..arr[1]..', "'..arr[2]..'", "'..arr[3]..'", "'..arr[4]..'", "'..arr[5]..'", "'..arr[6]..'", "'..arr[7]..'", '..arr[8]..', {'..math.random(0,11)..', '..arr[9]..'}, {'..math.random(0,11)..', '..endy..'}, "'..arr[11]..'"},\n'
 	end--]]
 	
-	--AddInventoryItem(thePlayer, "Деньги", 10000, 550, {})
+	--AddInventoryItem(thePlayer, "Кредитка", 1, 550, {})
 
 	
 	fileDelete("save.txt")
