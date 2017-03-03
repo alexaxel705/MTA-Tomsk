72483e985eb96b7536db9763b38f4f03d61eede4
Aleksei <Dark_ALEX@sibmail.com>
2017-03-03 13:09:54 +0700

    update

--- script.lua
+++ script.lua
@@ -4899,7 +4899,7 @@ function tp(thePlayer, command, h)
 		
 		--local x,y,z,i,d = int[2], int[3], int[4],int[1],0
 
-		local x,y,z,i,d  = 2109.7, -1466.8, 22.8, 0, 0 --
+		local x,y,z,i,d  = 153.2, -1552.5, 9.6, 0, 0 --
 		
 		if(theVehicle) then
 			SetPlayerPosition(theVehicle, x,y,z,i,d)
